a6c981caa1e0b991d82f9b31420a71cbd1f91519
Jacob <jacob-dan@hotmail.com>
2023-12-15 22:47:01 +0100

    feat: add navigation

--- Dracula/App.tsx
+++ Dracula/App.tsx
@@ -1,3 +1,107 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome to Dracula'}}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Create" component={CreateProfileScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={{ flexDirection:"row" }}>
+        <View style={styles.buttonStyle}>
+            <Button
+                  title="Login"
+                  onPress={() =>navigation.navigate('Login')}>
+                  Login
+            </Button>
+        </View>
+        <View style={styles.buttonStyle}>
+            <Button
+                title="Create profile"
+                onPress={() =>navigation.navigate('Create')}>
+                Create profile
+            </Button>
+        </View>
+    </View>
+  );
+};
+// onPress={() =>navigation.navigate('Profile', {name: 'Jane'})}
+
+const LoginScreen = ({navigation}) => {
+  return (
+  <View>
+    <Text>This is the login page, please enter your username.</Text>
+    <View style={styles.buttonStyle}>
+        <Button
+              title="Login"
+              onPress={() =>navigation.navigate('Profile', {username: 'Jane'})}
+              />
+    </View>
+  </View>
+  );
+};
+
+const CreateProfileScreen = ({navigation}) => {
+  return <Text>Create profile</Text>;
+};
+
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.username}'s profile</Text>;
+};
+
+
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  buttonStyle: {
+    marginHorizontal: 20,
+    marginTop: 5
+  },
+});
+
+export default App;
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,7 +109,11 @@
  * @format
  */
 
-import React from 'react';
+
+
+
+/*
+import * as React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -24,6 +132,11 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -55,6 +168,36 @@ function Section({children, title}: SectionProps): React.JSX.Element {
   );
 }
 
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+              <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{title: 'Welcome'}}
+              />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+            </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }/>
+  );
+};
+
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -71,7 +214,7 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        <Header>Hello</Header>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
@@ -96,6 +239,7 @@ function App(): React.JSX.Element {
   );
 }
 
+
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
@@ -116,3 +260,5 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+*/
