3ec3833317802bc7d5bf2a5698ff1072b86376fc
Swan <maillard.swan@gmail.com>
2023-12-17 11:57:12 +0100

    Score PBAC

--- Dracula/pages/QuestionResultScreen.tsx
+++ Dracula/pages/QuestionResultScreen.tsx
@@ -10,10 +10,24 @@ import { CommonActions } from '@react-navigation/native';
 const QuestionResultScreen = ({ navigation }) => {
   const [currentUser, setCurrentUser] = useStorage('DRACULA@current-user', '');
   const [cycles, setCycles, refreshCycles] = useStorage('DRACULA@cycles', {})
+  const [answersQuestions] = useStorage('DRACULA@answers-questions', {})
   const currentUserLC = currentUser.toLowerCase();
 
   let userCycles = cycles[currentUserLC] || []
   let lastCycle = userCycles.length > 0 ? userCycles[userCycles.length-1] : []
+
+  let lastAnswers = answersQuestions[currentUserLC]?.length > 0 ? answersQuestions[currentUserLC][answersQuestions[currentUserLC].length-1]: null;
+
+  let score = 0;
+
+  if (lastAnswers) {
+    if (lastAnswers[0]) score += 3;
+    if (lastAnswers[1]) score += 1;
+    if (lastAnswers[2]) score += 3;
+    if (lastAnswers[3]) score += 1;
+    if (lastAnswers[4]) score += 1;
+    if (lastAnswers[5]) score += 1;
+  }
 
   const handleClick = async () => {
     navigation.dispatch(CommonActions.reset({routes: [{ name: 'Profile'}]}))
@@ -44,7 +58,7 @@ const QuestionResultScreen = ({ navigation }) => {
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
-          gap: 30,
+          gap: 20,
           width: "75%"
       },
       title: {
@@ -69,10 +83,17 @@ const QuestionResultScreen = ({ navigation }) => {
           <View style={customStyles.container}>
             <TopWave/>
             <View style={customStyles.actions}>
-                <CustomText style={{fontSize: 20, marginBottom: 20}}>Your period lasted from</CustomText>
+                <CustomText style={{fontSize: 20}}>Your period lasted from</CustomText>
                 <Text style={customStyles.title}>{lastCycle[0]}</Text>
-                <CustomText style={{fontSize: 20, marginBottom: 5}}>To</CustomText>
+                <CustomText style={{fontSize: 20}}>To</CustomText>
                 <Text style={customStyles.title}>{lastCycle[lastCycle.length-1]}</Text>
+
+                <CustomText style={{fontSize: 20}}>You got {score} point(s)</CustomText>
+                { score > 3 ? (
+                    <CustomText style={{fontSize: 20}}>This might indicate that you have heavy menstrual bleeding.</CustomText>
+                ) : (
+                    <CustomText style={{fontSize: 20}}>This is a very healthy result!</CustomText>
+                )}
                 <Pressable style={{...styles.button, ...customStyles.loginButton}} onPress={handleClick}>
                     <CustomText style={{color: "white"}}>Back to profile</CustomText>
                 </Pressable>
