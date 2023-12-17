39740c3196d03fde6c6cea7be04e572c98b65447
Marie Hayashi Strand <marihstr@stud.ntnu.no>
2023-12-17 09:44:16 +0100

    Added PBACProvider

--- Dracula/hooks/useStorage.ts
+++ Dracula/hooks/useStorage.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { AsyncStorage } from '@react-native-async-storage/async-storage'
 
 interface IUseAsyncStorage {
   key?: string
