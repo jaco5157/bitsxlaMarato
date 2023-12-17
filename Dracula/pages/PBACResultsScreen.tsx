a3fc4880a6e4340db8d664004a001d28836b6bdb
Marie Hayashi Strand <marihstr@stud.ntnu.no>
2023-12-17 11:31:01 +0100

    Added PBACProvider

--- Dracula/pages/PBACResultsScreen.tsx
+++ Dracula/pages/PBACResultsScreen.tsx
@@ -8,7 +8,11 @@ import Svg, {Path} from 'react-native-svg'
 import { usePBACContext } from './PBACProvider';
 
 const PBACResultsScreen = ({ route, navigation }) => {
-    const { pbacAnswers, cumulativeScore } = usePBACContext();
+    const { pbacAnswers, cumulativeScore, resetValues } = usePBACContext();
+
+    const handleReset = () => {
+        resetValues();
+    }
 
     useEffect(() => {
         // This effect runs whenever 'answer' changes
@@ -25,6 +29,38 @@ const PBACResultsScreen = ({ route, navigation }) => {
            paddingTop: 5,
            width: "100%"
        },
+
+        headerText: {
+           fontSize: 20,
+           fontWeight: 'bold',
+        },
+        table: {
+           borderWidth: 1,
+           borderColor: '#000',
+           marginBottom: 16,
+        },
+        row: {
+           flexDirection: 'row',
+           borderBottomWidth: 1,
+           borderColor: '#000',
+        },
+        cellHeader: {
+           flex: 1,
+           padding: 8,
+           fontWeight: 'bold',
+        },
+        cell: {
+           flex: 1,
+           padding: 8,
+        },
+        footer: {
+           marginTop: 16,
+        },
+        footerText: {
+           fontSize: 18,
+           fontWeight: 'bold',
+        },
+
        container: {
          position: "relative",
          backgroundColor: colors.white,
@@ -99,8 +135,6 @@ const PBACResultsScreen = ({ route, navigation }) => {
        }
      })
 
-
-
    return (
      <View style={styles.body}>
        <View style={styles.mainContainer}>
@@ -112,9 +146,34 @@ const PBACResultsScreen = ({ route, navigation }) => {
              <TopWave/>
              <View style={customStyles.actions}>
                  <View style={customStyles.questionsContainer}>
-                     <CustomText style={{textAlign: "center"}}>{ cumulativeScore }</CustomText>
+                     {/* RESULTS TABLE */}
+                     <CustomText style={{textAlign: "center"}}>{ `PBAC Score: `, cumulativeScore }</CustomText>
+
+                          <View style={styles.container}>
+                            <View style={styles.header}>
+                              <Text style={styles.headerText}>Summary</Text>
+                            </View>
+                            <View style={styles.table}>
+                              <View style={styles.row}>
+                                <Text style={styles.cellHeader}> Choose product type: </Text>
+                                <Text style={styles.cellHeader}>Answer</Text>
+                              </View>
+                              {pbacAnswers.map((answer, index) => (
+                                <View key={index} style={styles.row}>
+                                  <Text style={styles.cell}>{`Question 1`}</Text>
+                                  <Text style={styles.cell}>{`Response`}</Text>
+                                </View>
+                              ))}
+                            </View>
+                            <View style={styles.footer}>
+                              <Text style={styles.footerText}>Cumulative Score: {cumulativeScore}</Text>
+                            </View>
+                          </View>
+
+                     {/* BACK BUTTON */}
                      <View style={customStyles.answers}>
                          <Pressable onPress={ () => {
+                                 handleReset();
                                  navigation.navigate('Home');
                          }} style={{...customStyles.answer, backgroundColor: colors.black}}>
                              <CustomText style={{color: "white"}}> BACK </CustomText>
@@ -126,7 +185,6 @@ const PBACResultsScreen = ({ route, navigation }) => {
        </View>
      </View>
     );
-
  };
 
 export default PBACResultsScreen;
